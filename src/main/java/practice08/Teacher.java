447fb7c6370e19262bc61dc5bbbdaea4dc3f4863
Amyoyzl <1421790940@qq.com>
2019-07-11 20:33:29 +0800

    edit practice8

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,38 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass != null)
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %d.", super.getName(), super.getAge(), klass.getNumber());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.", super.getName(), super.getAge());
+
+    }
+
+    public String introduceWith(Student student) {
+        if(student.getKlass().getNumber() == klass.getNumber())
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach %s.",
+                    super.getName(),super.getAge(),student.getName());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I don't teach %s.",
+                    super.getName(),super.getAge(),student.getName());
+    }
 }
+
