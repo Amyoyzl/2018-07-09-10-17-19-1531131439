930dbdfa9d7f97154dcfb1f150d1641b40bea5ab
Amyoyzl <1421790940@qq.com>
2019-07-11 20:49:13 +0800

    edit practice9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,33 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() != this)
+            System.out.print("It is not one of us.\n");
+        else
+            leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,27 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this == klass.getLeader())
+            return String.format("My name is %s. I am %d years old. I am a Student. I am Leader of Class %d.", super.getName(), super.getAge(), klass.getNumber());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Student. I am at Class %d.", super.getName(), super.getAge(), klass.getNumber());
+    }
 }
