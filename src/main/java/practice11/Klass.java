798c47bf3be6429e0f44967e7a3847da637f69c8
Amyoyzl <1421790940@qq.com>
2019-07-11 22:39:31 +0800

    edit practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,41 @@
 package practice11;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    private Teacher teacher;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() != this)
+            System.out.print("It is not one of us.\n");
+        else {
+            leader = student;
+            teacher.lookNewMessage(String.format("%s become Leader of Class %d.", student.getName(), this.number));
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        teacher.lookNewMessage(String.format("%s has joined Class %d.", student.getName(), this.number));
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,65 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        for(Klass klass : this.classes) {
+            klass.setTeacher(this);
+        }
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public void lookNewMessage(String message) {
+        System.out.println("I am Tom. I know "+ message);
+    }
+
+    @Override
+    public String introduce() {
+        if (classes == null)
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.",
+                    super.getName(), super.getAge());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %s.",
+                    super.getName(), super.getAge(), getClassesNumber());
+
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : classes) {
+            if (student.getKlass() == klass)
+                return true;
+        }
+        return false;
+    }
+
+    private String getClassesNumber() {
+        String classesString = "";
+        for (Klass klass : classes) {
+            classesString += klass.getNumber() + " ,";
+        }
+        return classesString.substring(0, classesString.length() - 3);
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student))
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach %s.",
+                    super.getName(), super.getAge(), student.getName());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I don't teach %s.",
+                    super.getName(), super.getAge(), student.getName());
+    }
+
 }
